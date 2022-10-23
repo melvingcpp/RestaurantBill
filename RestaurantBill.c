ddd3e9340b4e5df594c9f19569087a92d74c3a55
Melvin <melvingit@gmail.com>
2022-10-23 15:15:22 -0700

    created outline

---
+++ RestaurantBill.c
@@ -0,0 +1,8 @@
+
+//Create main function that can take in command line inputs
+
+//Write section that will randomly choose between the four meals and their costs
+
+//Compute the calculations to get the tax amount, tip amount, and total bill
+
+//Print all the values
